a7d3b53125d67b8cea80387037cf9abf9c307c99
sytrics <guillaume.leinen@ensta-bretagne.org>
2021-04-09 16:19:10 +0200

    fix

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+clc
 clear all 
 close all
 echo off 
@@ -73,31 +74,16 @@ dernier_round = moyenne(3057:3330);
 
 %% 5)prédiction d'etat sur la 1ere mesure (avant remontage sur point d'attaque) 
 % récupération du chiffré X_str
-A = strsplit(folderInfo(3).name, '_cto=');
-X_str = strtok(A{1,2}, '.');
-% conversion en chiffré manipulable
-X = ones(length(X_str)/2, 1);
-for i = 1:(length(X_str)/2)
-   X(i) = hex2dec(X_str(i:i+1));
+
+zattack = zeros(20000:16);
+for i = progress(3:20002)
+   zattack(i-2, :) = zattackFromFileName(fullfile(folderSrc, folderInfo(i).name));
 end
-% prédiction d'état après XOR 
-Z = bitxor(uint8(single(X)*ones(1,256)),uint8(ones(size(X,1),1)*(0:255)))+1; 
-
-% on remonte en appliquant une shiftrow inverse 
-
-Zshift = shiftrowinv(Z); 
-
-% on remonte encore pour arriver au point d'attaque 
-
-SBox=[99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,7,18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,208,239,170,251,67,77,51,133,69,249,2,127,80,60,159,168,81,163,64,143,146,157,56,245,188,182,218,33,16,255,243,210,205,12,19,236,95,151,68,23,196,167,126,61,100,93,25,115,96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,224,50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,186,120,37,46,28,166,180,198,232,221,116,31,75,189,139,138,112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,158,225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,140,161,137,13,191,230,66,104,65,153,45,15,176,84,187,22];
-invSBox(SBox(1:256)+1)=0:255;
-
-Zattack = invSBox(Zshift);
 
 %% attaque par HW
 
-z_10=de2bi(Zattack,8,'left-msb');
-HW = uint8(reshape(sum(z_10,2),size(Zattack,1),size(Zattack,2)));
+z_10=de2bi(zattack,8,'left-msb');
+HW = uint8(reshape(sum(z_10,2),size(zattack,1),size(zattack,2)));
 phi = HW; 
 
 L = load(fullfile(folderSrc, folderInfo(3).name), "-mat").data;
