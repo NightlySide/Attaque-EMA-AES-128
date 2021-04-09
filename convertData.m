8c11f38e0a8d2a4600c4c1900ff794fe693c182a
nightlyside <nightlyside@gmail.com>
2021-04-09 16:29:28 +0200

    adding all traces in single file

--- convertData.m
+++ convertData.m
@@ -6,6 +6,7 @@ function convertData()
     folderSrc = fullfile(pwd, "data");
     csvFiles = dir(fullfile(folderSrc, "*.csv")); 
     folderDst = fullfile(pwd, "data_converted");
+    cacheFolder = fullfile(pwd, "cache");
 
     %% converting only if it hasn't been done before
     if size(dir(folderDst), 1) < length(csvFiles) + 2
@@ -16,5 +17,16 @@ function convertData()
         end
     else
         disp("No need to convert data as it has already be done.")
+    end
+    
+    %% creating a single file with all data
+    if exist(fullfile(cacheFolder, "fuites.mat"), "file") == 0
+        disp("Creating a single file with all data into it")
+        fuites = zeros(length(csvFiles), 4000);
+        for j = progress(1:length(csvFiles))
+            fuites(j, :) = load(fullfile(folderDst, csvFiles(j).name), '-mat').data;
+        end
+        disp("Saving file to cache")
+        save(fullfile(cacheFolder, "fuites.mat"), "fuites");
     end
 end
