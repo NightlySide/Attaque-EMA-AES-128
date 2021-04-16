37d87815373248abc0b6e5c5cf7c612261c646f5
nightlyside <nightlyside@gmail.com>
2021-04-16 18:35:58 +0200

    Fixing xor

--- main.m
+++ main.m
@@ -105,29 +105,47 @@ for k = 3:(Ntraces+2)
     end
 end
 
-Z = uint8(zeros(Ntraces,256,16));
+Z    = uint8(zeros(Ntraces,256,16));
+Z_sr = uint8(zeros(Ntraces,256,16));
+Z_sb = uint8(zeros(Ntraces,256,16));
 
 disp("Remplissage de l'état Z")
-for j = 1:256 
-    Z(:,j,:) = X(1:Ntraces, :);
-end 
+for trace = 1:Ntraces
+    for hypothese = 1:256
+        for valeur = 1:16
+            Z(trace, hypothese, valeur) = X(trace, valeur);
+        end 
+    end 
+end
 
 % ----- Valide au dessus de ce point
 
 % xor 
 disp("XOR sur Z")
-possibilites = int8(0:255);
 for trace = 1:Ntraces
-   for hypothese = 1:16
-      Z(trace, :, hypothese) = bitxor(Z(trace, :, hypothese), uint8(possibilites)); 
+   for valeur = 1:16
+       for hypothese = 1:256
+           Z(trace, hypothese, valeur) = bitxor(Z(trace, hypothese, valeur), uint8(hypothese-1)); 
+       end
    end
 end
+disp("X")
+disp(X(1, 1))
+disp("Z")
+disp(Z(1, :, 1))
 
 %shiftrow
 shiftRowInv = [1, 14, 11, 8, 5, 2, 15, 12, 9, 6, 3, 16, 13, 10, 7, 4];
 
 disp("ShiftRow inverse")
-Z_sr = Z(:,:, shiftRowInv); 
+for trace = 1:Ntraces
+    for valeur = 1:16
+       for hypothese = 1:256
+           Z_sr(trace, hypothese, valeur) = Z(trace, hypothese, shiftRowInv(valeur)); 
+       end
+   end
+end
+%Z_sr = Z(:, :, shiftRowInv); 
 
 % on remonte encore pour arriver au point d'attaque 
 SBox=[99,124,119,123,242,107,111,197,48,1,103,43,254,215,171,118,202,130,201,125,250,89,71,240,173,212,162,175,156,164,114,192,183,253,147,38,54,63,247,204,52,165,229,241,113,216,49,21,4,199,35,195,24,150,5,154,7,18,128,226,235,39,178,117,9,131,44,26,27,110,90,160,82,59,214,179,41,227,47,132,83,209,0,237,32,252,177,91,106,203,190,57,74,76,88,207,208,239,170,251,67,77,51,133,69,249,2,127,80,60,159,168,81,163,64,143,146,157,56,245,188,182,218,33,16,255,243,210,205,12,19,236,95,151,68,23,196,167,126,61,100,93,25,115,96,129,79,220,34,42,144,136,70,238,184,20,222,94,11,219,224,50,58,10,73,6,36,92,194,211,172,98,145,149,228,121,231,200,55,109,141,213,78,169,108,86,244,234,101,122,174,8,186,120,37,46,28,166,180,198,232,221,116,31,75,189,139,138,112,62,181,102,72,3,246,14,97,53,87,185,134,193,29,158,225,248,152,17,105,217,142,148,155,30,135,233,206,85,40,223,140,161,137,13,191,230,66,104,65,153,45,15,176,84,187,22];
@@ -135,7 +153,14 @@ invSBox(SBox(1:256)+1) = 0:255;
 
 disp("Inv subbyte")
 % on passe de 0-255 --> 1-256
-Z_sb = invSBox(Z_sr + 1);
+%Z_sb = invSBox(Z_sr + 1);
+for trace = 1:Ntraces
+    for valeur = 1:16
+       for hypothese = 1:256
+           Z_sb(trace, hypothese, valeur) = uint8(invSBox(Z_sr(trace, hypothese, valeur)+1)); 
+       end
+   end
+end
 
 % cleaning
 % clearvars A cle file_name i j k trace X_str Z_sr Z
@@ -165,7 +190,7 @@ for k = 1:16
     
     if k == 1
         figure 
-        plot ((dernier_round), cor(:, dernier_round))
+        plot(dernier_round, cor(:, dernier_round))
         title('bcp de coef de correlation')
         xlabel('echantillon')
         ylabel('correlation')
